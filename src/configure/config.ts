aaa34e8038d227c36739283ebd9690fe9de808af
geo653rge <georgekimanikiringa@gmail.com>
2024-03-06 11:54:28 +0300

    added configs to project

---
+++ src/configure/config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+
+export const CONFIG = {
+BYBIT_API_KEY:process.env.BYBIT_API_KEY || '',
+
+BYBIT_API_SECRET:process.env.BYBIT_API_SECRET || '',
+BYBIT_TESTNET: 'true',
+
+BOT_TOKEN: process.env.BOT_TOKEN ||'',
+
+
+
+
+}
