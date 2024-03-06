a89f83327c7df917bd0f68f119d113400e01a78a
geo653rge <georgekimanikiringa@gmail.com>
2024-03-06 11:35:33 +0300

    added a server

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express() ;
+
+
+
+const Main = async () => {
+
+try {
+}
+
+catch (error) {
+
+    console.log(error);
+}
+};
+
+Main();
+app.listen(8000, () => {
+console.log(" Server running on port 8000");
+
+});
